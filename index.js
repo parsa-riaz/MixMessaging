a0e55118e456c4d08b9553b7981f90a2b4d6454e
parsa-riaz <riazparsa1@gmail.com>
2023-10-01 20:02:18 +0500

    version 1

--- index.js
+++ index.js
@@ -23,8 +23,6 @@ let speaker = document.getElementById("speaker");
 document.querySelector("#start").onclick = () => {
   if (start === false) {
     audio.play();
-    audio.loop = true;
-
     speaker.src = "./images/sound.png";
     start = true;
   } else if (start === true) {
